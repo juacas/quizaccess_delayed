c383fe6d7051228a25387a61e31f24e96464213b
Juan Pablo <juan.pablo.de.castro@gmail.com>
2020-04-27 10:59:50 +0200

    v1.1.0 release

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 
 defined ( 'MOODLE_INTERNAL' ) || die ();
 
-$plugin->version = 2020042600;
+$plugin->version = 2020042700;
 $plugin->requires = 2017050500;
 $plugin->component = 'quizaccess_activatedelayedattempt';
 $plugin->maturity  = MATURITY_RC;
