7a25ea6be5d369c5068ffc95448a8aa5f4764c86
Juan Pablo de Castro <juan.pablo.de.castro@gmail.com>
2025-03-11 10:54:27 +0000

    v1.2.8  Fix Attempt button not behaving as native quiz's one.

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 
 defined ( 'MOODLE_INTERNAL' ) || die ();
 
-$plugin->version = 2025030700;
+$plugin->version = 2025031100;
 $plugin->requires = 2017050500;
 $plugin->component = 'quizaccess_delayed';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.2.7';
+$plugin->release   = '1.2.8';
